121a9b51480e4594c1925fb9dabe269b4bed9c0e
Maciej Kozik <kamac2495@gmail.com>
2018-03-13 12:35:33 +0100

    add a working model manipulation example

--- api/bus.js
+++ api/bus.js
@@ -1,22 +1,30 @@
 var router = require('express').Router();
+const async = require('async');
 const { Vehicle, VehiclePosition } = require('../models/');
 
 router.get('/', function(req, res, next) {
-  Vehicle.findAll().then((results) => {
-    res.send(results);
+  Vehicle.findAll().then((vehicles) => {
+    res.send(vehicles);
   });
 })
 
 router.get('/test', function(req, res, next) {
+  Vehicle.destroy({ where: {} });
+
   Vehicle.create({
     name: 'A',
     vehicleType: 'Bus'
   }).then(v => {
-    v.setPositions(Array(10).map(x => VehiclePosition.create({
-      x: Math.random() * 10,
-      y: Math.random() * 10,
-      k: 1
-    }))).then(() => res.send(v));
+    async.series(Array(10).fill(null).map(x => (callback) => {
+      VehiclePosition.create({
+        x: Math.random() * 10,
+        y: Math.random() * 10,
+        k: 1,
+        posDate: new Date()
+      }).then((p) => callback(null, p), (err) => callback(err));
+    }), (err, results) => {
+      v.setPositions(results).then(() => res.send(v));
+    });
   });
 })
 
