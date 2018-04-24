92386d12cd73d045e1df8661be044deeba75be4c
Maciej Kozik <kamac2495@gmail.com>
2018-04-24 08:42:54 +0200

    allow selecting specific bus to show more of his position history

--- api/bus.js
+++ api/bus.js
@@ -4,63 +4,67 @@ const { Vehicle, VehiclePosition } = require('../models/');
 const { Op } = require('sequelize');
 var moment = require('moment');
 
-router.get('/', function(req, res, next) {
-  VehiclePosition.findAll({
-    where: {
-      createdAt: {
-        [Op.gte]: moment().subtract(1, 'minutes').toDate()
-      }
-    },
-    order: [
-      ['posDate', 'DESC']
-    ]
-  }).then((vehiclePositions) => {
+router.get('/:id?', async (req, res, next) => {
+  if(req.params.id)
+    res.params.id = parseInt(req.params.id);
+  else
+    req.params.id = -1;
+
+  try {
+    let vehiclePositions = await VehiclePosition.findAll({
+      where: {
+          [Op.or]: [
+            {
+              createdAt: {
+                [Op.gte]: moment().subtract(10, 'minutes').toDate()
+              },
+              VehicleId: {
+                [Op.eq]: req.params.id
+              }
+            },
+            {
+              createdAt: {
+                [Op.gte]: moment().subtract(40, 'seconds').toDate()
+              },
+              VehicleId: {
+                [Op.ne]: req.params.id
+              }
+            }
+          ]
+      },
+      order: [
+        ['posDate', 'DESC']
+      ]
+    });
+
     let vehicleIds = [];
     for(let i = 0; i < vehiclePositions.length; i++) {
       if(vehicleIds.indexOf(vehiclePositions[i].VehicleId) === -1) {
         vehicleIds.push(vehiclePositions[i].VehicleId);
       }
     }
-    Vehicle.findAll({
+    let vehicles = await Vehicle.findAll({
       where: {
         id: {
           [Op.in]: vehicleIds
         }
       }
-    }).then(vehicles => {
-      let answer = vehicles.map(v => { return {
-        name: v.name,
-        model: v.model,
-        positions: vehiclePositions.filter(p => p.VehicleId == v.id).map(p => { return {
-          x: p.x,
-          y: p.y,
-          posDate: p.posDate
-        }})
-      }});
-      res.send(answer);
-    })
-  });
-});
-
-/*router.get('/test', function(req, res, next) {
-  // destroy all vehicles
-  Vehicle.destroy({ where: {} });
-
-  // create a new vehicle, then fill it's positions with 10 random positions
-  Vehicle.create({
-    name: 'A',
-    vehicleType: 'Bus'
-  }).then(v => {
-    async.series(Array(10).fill(null).map(x => (callback) => {
-      VehiclePosition.create({
-        x: Math.random() * 10,
-        y: Math.random() * 10,
-        posDate: new Date()
-      }).then((p) => callback(null, p), (err) => callback(err));
-    }), (err, results) => {
-      v.setPositions(results).then(() => res.send(v));
     });
-  });
-})*/
+
+    let answer = vehicles.map(v => { return {
+      id: v.id,
+      name: v.name,
+      model: v.model,
+      positions: vehiclePositions.filter(p => p.VehicleId == v.id).map(p => { return {
+        x: p.x,
+        y: p.y,
+        posDate: p.posDate
+      }})
+    }});
+    res.send(answer);
+  } catch (e) {
+    next(e);
+  }
+});
 
 module.exports = router;
